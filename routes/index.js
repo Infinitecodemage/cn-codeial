d1f27ee321d02875198adfa9063da7cce64e60c2
mageraj <prdRiteshRaj@gmail.com>
2023-03-14 20:54:31 +0530

    m 6 l 8 controller and router for users_controller

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
+
+
 console.log(`router loaded`);
 
 //function in home_controllers.js
 router.get('/', homeController.home);
+
+// router.use
+router.use('/users', require('./users'));
+// It is little bit different from home_controllers
+// I think for home_controllers is directly access by this router.
+// No, that part is done in users.js
+
+
 
 module.exports = router;
 
